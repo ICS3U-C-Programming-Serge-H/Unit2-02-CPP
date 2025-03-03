96782eb0af23c800b629d75cbda5d85e8e584fd5
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-03-03 01:10:14 +0000

    commit areaPerRectInput.cpp

---
+++ areaPerRectInput.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Serge All rights reserved
+// Created by Serge Hamouche
+// Created on Feb 2025
+// This program calculates the area and perimeter of a rectangle
+
+#include <iostream>
+int main() {
+    int length;
+    std::cout << "Enter length of the rectangle (mm): 9 " << std::endl;
+    std::cin >> length;
+    int width;
+    std::cout << "Enter width of the rectangle (mm): 4 " << std::endl;
+    std::cin >> width;
+}
